ae5e236ab3c3d01ff2b531000161c287f40fa9b6
Semyon <semhowork@gmail.com>
2024-02-16 06:55:52 +0300

    templates

--- platform/placeholder/message_middleware.go
+++ platform/placeholder/message_middleware.go
@@ -1,26 +1,33 @@
 package placeholder
 
 import (
-	"errors"
-	"io"
 	"platform/config"
 	"platform/pipeline"
 	"platform/services"
+	"platform/templates"
 )
 
-type SimpleMessageComponent struct{}
+type SimpleMessageComponent struct {
+	Message string
+	config.Configuration
+}
 
-func (c *SimpleMessageComponent) Init() {}
+func (c *SimpleMessageComponent) Init() {
+	services.GetService(&c.Configuration)
+	c.Message = c.Configuration.GetStringDefault("main:message", "Default Message")
+}
 
-func (c *SimpleMessageComponent) ProcessRequest(ctx *pipeline.ComponentContext, next func(*pipeline.ComponentContext)) {
-	var cfg config.Configuration
-	services.GetService(&cfg)
+func (c *SimpleMessageComponent) ImplementsProcessRequestWithServices() {}
 
-	msg, ok := cfg.GetString("main:message")
-	if !ok {
-		ctx.Error(errors.New("Cannot find config setting"))
+func (c *SimpleMessageComponent) ProcessRequestWithServices(
+	ctx *pipeline.ComponentContext,
+	next func(*pipeline.ComponentContext),
+	executor templates.TemplateExecutor) {
+	err := executor.ExecTemplate(ctx.ResponseWriter,
+		"simple_message.html", c.Message)
+	if err != nil {
+		ctx.Error(err)
 	} else {
-		io.WriteString(ctx.ResponseWriter, msg)
+		next(ctx)
 	}
-	next(ctx)
 }
