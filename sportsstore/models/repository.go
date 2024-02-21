a9e4d2ecfbbb59c56be78498d6fb708269c048ef
Semyon <semhowork@gmail.com>
2024-02-21 07:56:57 +0300

    added pagination

--- sportsstore/models/repository.go
+++ sportsstore/models/repository.go
@@ -3,6 +3,7 @@ package models
 type Repository interface {
 	GetProduct(id int) Product
 	GetProducts() []Product
+	GetProductPage(page, pageSize int) (product []Product, totalAvailable int)
 	GetCategories() []Category
 	Seed()
 }
